b47f656ce6c75af3de7b1962c06e8483e7201c7e
alexandrkerzhakov <alexandr.kerzhakov35@gmail.com>
2022-02-05 20:31:26 +0300

    test

--- passw_gen/settings.py
+++ passw_gen/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#i(3&8u&tc4&^a0a7c(pyh@wha*_6gt^2p573ak$s9ij5g4*%p'
+SECRET_KEY = 'asasasdjango-insecure-#i(3&8u&tc4&^a0a7c(pyh@wha*_6gt^2p573ak$s9ij5g4*%p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
